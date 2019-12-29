49ed1f7d041e0c43894e2c1d6a407962d30a7f4e
Pok Tik Benajmin Leung <benminleung@gmail.com>
2019-12-29 04:29:36 +0200

    Updates

---
+++ precache-manifest.8f40805372a6e501d6475de2e0aea96d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4af409989b3261a738f878569bd27b7",
+    "url": "/triominos-calculator/index.html"
+  },
+  {
+    "revision": "6206d8d2efcfcd87f63c",
+    "url": "/triominos-calculator/static/css/main.f8072643.chunk.css"
+  },
+  {
+    "revision": "74fd7c7e193787d1be8a",
+    "url": "/triominos-calculator/static/js/2.b9af38cd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/triominos-calculator/static/js/2.b9af38cd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6206d8d2efcfcd87f63c",
+    "url": "/triominos-calculator/static/js/main.284b2f47.chunk.js"
+  },
+  {
+    "revision": "b5db363dab0b7f09e83a",
+    "url": "/triominos-calculator/static/js/runtime-main.af33708c.js"
+  }
+]);
